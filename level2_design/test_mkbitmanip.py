e3c4de75375d5057d684479e308487c40825ff04
tanyatyagi35 <tanyatyagi35@gmail.com>
2022-08-01 01:57:14 +0530

    Update test_mkbitmanip.py

--- level2_design/test_mkbitmanip.py
+++ level2_design/test_mkbitmanip.py
@@ -22,23 +22,21 @@ def clock_gen(signal):
 
 # Sample Test
 @cocotb.test()
-def run_test(dut):
-
+def run_test1(dut):
     # clock
     cocotb.fork(clock_gen(dut.CLK))
-
     # reset
     dut.RST_N.value <= 0
     yield Timer(10) 
     dut.RST_N.value <= 1
-
     ######### CTB : Modify the test to expose the bug #############
     # input transaction
-    # for all the non zero values of mav_putvalue_src2 and mav_putvalue_src3, the test will fail
-    mav_putvalue_src1 = 0x5
-    mav_putvalue_src2 = 0x1
-    mav_putvalue_src3 = 0x0
-    mav_putvalue_instr = 0x101010B3
+    #case1: for zero values of both mav_putvalue_src1 & mav_putvalue_src2 and non-zero value of mav_outvalue_src3, the test will fail
+    #applicable for all possible instructions
+    mav_putvalue_src1 = 0x0
+    mav_putvalue_src2 = 0x0
+    mav_putvalue_src3 = 0xFFFF1111
+    mav_putvalue_instr = 0x00100000
 
     # expected output from the model
     expected_mav_putvalue = bitmanip(mav_putvalue_instr, mav_putvalue_src1, mav_putvalue_src2, mav_putvalue_src3)
@@ -51,13 +49,124 @@ def run_test(dut):
     dut.mav_putvalue_instr.value = mav_putvalue_instr
   
     yield Timer(1) 
-
     # obtaining the output
     dut_output = dut.mav_putvalue.value
-
     cocotb.log.info(f'DUT OUTPUT={hex(dut_output)}')
     cocotb.log.info(f'EXPECTED OUTPUT={hex(expected_mav_putvalue)}')
-    
+
+    # comparison
+    error_message = f'Value mismatch DUT = {hex(dut_output)} does not match MODEL = {hex(expected_mav_putvalue)}'
+    assert dut_output == expected_mav_putvalue, error_message
+
+
+@cocotb.test()
+def run_test2(dut):
+    # clock
+    cocotb.fork(clock_gen(dut.CLK))
+    # reset
+    dut.RST_N.value <= 0
+    yield Timer(10) 
+    dut.RST_N.value <= 1
+    ######### CTB : Modify the test to expose the bug #############
+    # input transaction
+    #case2: for non-zero values of both mav_putvalue_src1 & mav_putvalue_src3 and zero value for mav_outvalue_src3, the test will fail
+    #applicable for all possible insrtructions 
+    mav_putvalue_src1 = 0x1F
+    mav_putvalue_src2 = 0x0
+    mav_putvalue_src3 = 0xFFFF1111
+    mav_putvalue_instr = 0x10100100
+
+    # expected output from the model
+    expected_mav_putvalue = bitmanip(mav_putvalue_instr, mav_putvalue_src1, mav_putvalue_src2, mav_putvalue_src3)
+
+    # driving the input transaction
+    dut.mav_putvalue_src1.value = mav_putvalue_src1
+    dut.mav_putvalue_src2.value = mav_putvalue_src2
+    dut.mav_putvalue_src3.value = mav_putvalue_src3
+    dut.EN_mav_putvalue.value = 1
+    dut.mav_putvalue_instr.value = mav_putvalue_instr
+  
+    yield Timer(1) 
+    # obtaining the output
+    dut_output = dut.mav_putvalue.value
+    cocotb.log.info(f'DUT OUTPUT={hex(dut_output)}')
+    cocotb.log.info(f'EXPECTED OUTPUT={hex(expected_mav_putvalue)}')
+
+    # comparison
+    error_message = f'Value mismatch DUT = {hex(dut_output)} does not match MODEL = {hex(expected_mav_putvalue)}'
+    assert dut_output == expected_mav_putvalue, error_message
+
+@cocotb.test()
+def run_test3(dut):
+    # clock
+    cocotb.fork(clock_gen(dut.CLK))
+    # reset
+    dut.RST_N.value <= 0
+    yield Timer(10) 
+    dut.RST_N.value <= 1
+    ######### CTB : Modify the test to expose the bug #############
+    # input transaction
+    #case3: for non-zero values of both mav_putvalue_src1 & mav_putvalue_src2 and zero value of mav_outvalue_src3, the test will fail
+    #applicable for all possible instructions
+    mav_putvalue_src1 = 0x1F
+    mav_putvalue_src2 = 0xFF2
+    mav_putvalue_src3 = 0x0
+    mav_putvalue_instr = 0x10100010
+
+    # expected output from the model
+    expected_mav_putvalue = bitmanip(mav_putvalue_instr, mav_putvalue_src1, mav_putvalue_src2, mav_putvalue_src3)
+
+    # driving the input transaction
+    dut.mav_putvalue_src1.value = mav_putvalue_src1
+    dut.mav_putvalue_src2.value = mav_putvalue_src2
+    dut.mav_putvalue_src3.value = mav_putvalue_src3
+    dut.EN_mav_putvalue.value = 1
+    dut.mav_putvalue_instr.value = mav_putvalue_instr
+  
+    yield Timer(1) 
+    # obtaining the output
+    dut_output = dut.mav_putvalue.value
+    cocotb.log.info(f'DUT OUTPUT={hex(dut_output)}')
+    cocotb.log.info(f'EXPECTED OUTPUT={hex(expected_mav_putvalue)}')
+
+    # comparison
+    error_message = f'Value mismatch DUT = {hex(dut_output)} does not match MODEL = {hex(expected_mav_putvalue)}'
+    assert dut_output == expected_mav_putvalue, error_message
+
+
+@cocotb.test()
+def run_test4(dut):
+    # clock
+    cocotb.fork(clock_gen(dut.CLK))
+    # reset
+    dut.RST_N.value <= 0
+    yield Timer(10) 
+    dut.RST_N.value <= 1
+    ######### CTB : Modify the test to expose the bug #############
+    # input transaction
+    #case4: for non-zero values of all three inputs i.e., mav_putvalue_src1, mav_putvalue_src2 and mav_outvalue_src3, the test will fail
+    #applicable for all possible instructions
+    mav_putvalue_src1 = 0x1
+    mav_putvalue_src2 = 0x7F
+    mav_putvalue_src3 = 0xFFFF1111
+    mav_putvalue_instr = 0x11100B20
+
+    # expected output from the model
+    expected_mav_putvalue = bitmanip(mav_putvalue_instr, mav_putvalue_src1, mav_putvalue_src2, mav_putvalue_src3)
+
+    # driving the input transaction
+    dut.mav_putvalue_src1.value = mav_putvalue_src1
+    dut.mav_putvalue_src2.value = mav_putvalue_src2
+    dut.mav_putvalue_src3.value = mav_putvalue_src3
+    dut.EN_mav_putvalue.value = 1
+    dut.mav_putvalue_instr.value = mav_putvalue_instr
+  
+    yield Timer(1) 
+    # obtaining the output
+    dut_output = dut.mav_putvalue.value
+    cocotb.log.info(f'DUT OUTPUT={hex(dut_output)}')
+    cocotb.log.info(f'EXPECTED OUTPUT={hex(expected_mav_putvalue)}')
+
     # comparison
     error_message = f'Value mismatch DUT = {hex(dut_output)} does not match MODEL = {hex(expected_mav_putvalue)}'
     assert dut_output == expected_mav_putvalue, error_message
